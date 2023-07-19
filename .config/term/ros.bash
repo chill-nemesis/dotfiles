efac0567d8d21840fdc4839f5b8b3eb5f97d4840
Steffen Peikert <steffen.peikert@fau.de>
2023-07-19 14:57:46 +0200

    Add colcon auto-complete for bash

---
+++ .config/term/ros.bash
@@ -0,0 +1,6 @@
+# TODO: check which ros is installed
+
+# Add colcon argcompletion (ros 2 build tool)
+if [ -f /usr/share/colcon_argcomplete/hook/colcon-argcomplete.bash ]; then
+	. /usr/share/colcon_argcomplete/hook/colcon-argcomplete.bash
+fi
