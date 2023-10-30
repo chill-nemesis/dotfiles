8fec7c7c1e22c41c0148ac0fe8f52b24a8f59ac3
Steffen Peikert <steffen.peikert@fau.de>
2023-10-30 09:50:23 +0100

    Move helper functions to dedicated file

--- install.sh
+++ install.sh
@@ -1,18 +1,5 @@
-function convert_to_windows ()
-{
-	local path=$1
-
-	# Check if we have an absolute path
-	if [[ $path == /* ]]; then
-		drive_letter=$(echo "$path" | cut -d '/' -f 2)
-		path_without_drive=$(echo "$path" | sed 's|/||' | cut -c2-)
-		echo "${drive_letter}:${path_without_drive//\//\\}"
-	else
-		# relative, so no drive letter extraction
-		echo "${path//\//\\}"
-	fi
-}
-
+# load helper functions
+source term/helpers.rc
 
 
 DF_ROOT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
@@ -21,18 +8,8 @@ DF_INSTALL_CACHE_DIR=$DF_ROOT_DIR/.cache/install
 # A list of files that need to be moved for install
 DF_FILES_TO_MOVE=".bashrc .zshrc .vimrc .tmux.conf .gitconfig"
 
-# check if we are running on a windows machine
-# https://stackoverflow.com/questions/3466166/how-to-check-if-running-in-cygwin-mac-or-linux
-system_name="$(uname -s)"
-is_windows=false
-case "${unameOut}" in
-    CYGWIN*)    is_windows=true;;
-    MINGW*)     is_windows=true;;
-    MSYS_NT*)   is_windows=true;;
-    *)          ;; # e.g. linux, darwin, or any other system -> Assume we work as linux
-esac
 
-if [ is_windows ]; then
+if is_windows; then
 	echo "To install, you need to run the shell as an administrator!"
 	while true; do
 		read -p "Do you want to continue? [Y/n] " yn
@@ -71,8 +48,8 @@ for file in $DF_FILES_TO_MOVE; do
 	mv ~/$file $DF_INSTALL_CACHE_DIR/$file 2>/dev/null
 
 	if [ is_windows ]; then
-		target=$(convert_to_windows ${target})
-		link=$(convert_to_windows ${link})
+		target=$(convert_path_to_windows ${target})
+		link=$(convert_path_to_windows ${link})
 
 		cmd <<< "mklink \"${link}\" \"${target}\"" > /dev/null
 	else
@@ -84,8 +61,6 @@ done
 echo "Almost done. Restarting shell to finish installation..."
 
 exec "$SHELL"
-
-unset DF_ROOT_DIR DF_INSTALL_CACHE_DIR DF_FILES_TO_MOVE
 
 
 
